00d203ffeda0af746e75987849600125392f7681
Sathya7411 <snsathya7411@gmail.com>
2022-01-04 21:30:17 -0800

    added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// department
+// departments sathya
 
 var department = require('./models/department.js');
 var departmentApi = require('./router/department-Api.js');

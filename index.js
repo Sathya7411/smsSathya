1b81a734b9a0c1b4c27c29ed06d73a09855a52e7
Sathya7411 <snsathya7411@gmail.com>
2022-01-04 22:34:58 -0800

    updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 
 // departments sathya
-
+//department schema
 var department = require('./models/department.js');
 var departmentApi = require('./router/department-Api.js');
 
